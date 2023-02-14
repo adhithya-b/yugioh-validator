fec07cb355ef45052a37da57c590dee49e239876
= <adhithya.balaji98@gmail.com>
2023-02-13 22:50:39 -0600

    adding react front end for edopro

--- app.py
+++ app.py
@@ -1,28 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, send_from_directory
 from validator import validate
+from flask_cors import CORS
 import os
-app = Flask(__name__)
-@app.route("/home", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        resp = []
-        if request.form['deckType'] == 'edopro':
-            resp = validate(request.form['deckType'], deckListString=request.form['decklist'])
-        elif request.form['deckType'] == 'duelingbook':
-            f = request.files['dbFile']
-            if f:
-                f.save(f.filename)
-                resp = validate(request.form['deckType'], deckFile=f.filename)
-                os.remove(f.filename)
-        else:
-            return render_template('home.html', status="Input is faulty.")
 
-        if resp:
-            return render_template('home.html', responses=resp)
-        else:
-            return render_template('home.html', status="Your deck is valid.")
+app = Flask(__name__, static_folder='build', template_folder='build', static_url_path='/')
+CORS(app)
+
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+    return {'message': 'test'}
+
+@app.route("/")
+def index():
+    print("displaying index.html")
+    # return send_from_directory(app.static_folder, 'index.html')
+    return app.send_static_file('index.html')
+
+@app.route("/validateDeck", methods=['POST'])
+def validateDeck():
+    print("validating deck")
+    resp = []
+    data = request.get_json()
+    if data['deckType'] == 'edopro':
+        resp = validate(data['deckType'], deckListString=data['decklist'])
+    elif data['deckType'] == 'duelingbook':
+        f = request.files['dbFile']
+        if f:
+            f.save(f.filename)
+            resp = validate(data['deckType'], deckFile=f.filename)
+            os.remove(f.filename)
     else:
-        return render_template('home.html')
+        return ["Input is faulty."]
+
+    print(resp)
+    if resp:
+        return resp
+    else:
+        return ["Your deck is valid."]
 
 if __name__ == '__main__':
     app.run(debug=True)
